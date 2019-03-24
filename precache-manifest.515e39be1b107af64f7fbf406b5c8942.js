9aadb3b163c93fa38cab4d3f5ec580c9e53700ac
Abdul Khuddus <khuddus1@gmail.com>
2019-03-25 00:43:41 +0530

    Updates

---
+++ precache-manifest.515e39be1b107af64f7fbf406b5c8942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "351fc99354337a05b0be",
+    "url": "/snake-game/static/js/runtime~main.351fc993.js"
+  },
+  {
+    "revision": "edd8d93acc228c375720",
+    "url": "/snake-game/static/js/main.edd8d93a.chunk.js"
+  },
+  {
+    "revision": "5345b3b9040eda074766",
+    "url": "/snake-game/static/js/1.5345b3b9.chunk.js"
+  },
+  {
+    "revision": "edd8d93acc228c375720",
+    "url": "/snake-game/static/css/main.79a5e11d.chunk.css"
+  },
+  {
+    "revision": "44996761f4a763765686f62f445c0c38",
+    "url": "/snake-game/index.html"
+  }
+];
